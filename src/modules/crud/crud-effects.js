565dd620e64dd57ae6b4e207cf0d01cd59aa393c
Tomas Weiss <tomasw@salsitasoft.com>
2017-10-27 13:45:24 +0200

    fixing review comments

--- src/modules/crud/crud-effects.js
+++ src/modules/crud/crud-effects.js
@@ -3,8 +3,8 @@ import wrapApiCall from "modules/api/wrap-api-call";
 
 export const getUsers = wrapApiCall(() => apiClient.get("/users"));
 export const getUser = wrapApiCall(id => apiClient.get(`/users/${id}`));
-export const addUser = wrapApiCall(user => apiClient.put("/users", user));
+export const addUser = wrapApiCall(user => apiClient.post("/users", user));
 export const deleteUser = wrapApiCall(id => apiClient.delete(`/users/${id}`));
 export const updateUser = wrapApiCall((data, id) =>
-  apiClient.post(`/users/${id}`, data)
+  apiClient.patch(`/users/${id}`, data)
 );
