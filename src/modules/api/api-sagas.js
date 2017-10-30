6d6fdf4ebc75fb16ce22376ed4d4367c3f5d4abf
Tomas Weiss <tomasw@salsitasoft.com>
2017-10-30 12:44:14 +0100

    docs

--- src/modules/api/api-sagas.js
+++ src/modules/api/api-sagas.js
@@ -3,6 +3,13 @@ import { call, put } from "redux-saga/effects";
 import Actions from "modules/api/api-actions";
 import { ApiError } from "modules/api/api-errors";
 
+/**
+ * Wraps saga into try/catch block catching all the ApiErrors
+ * and translating them to API_ERROR actions.
+ *
+ * @param {Function*} API call saga which may throw ApiError 
+ * @param {...Any} arguments to be passed to the saga 
+ */
 export function* withErrorHandling(saga, ...args) {
   try {
     return yield call(saga, ...args);
