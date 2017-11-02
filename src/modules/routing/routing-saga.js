a1fdc943df9bc6cfa410d2016a2380e54a207f0b
Tomas Weiss <tomasw@salsitasoft.com>
2017-11-02 12:44:33 +0100

    tests

--- src/modules/routing/routing-saga.js
+++ src/modules/routing/routing-saga.js
@@ -2,6 +2,13 @@ import transitionPath from "router5.transition-path";
 import { fork, take } from "redux-saga/effects";
 import { actionTypes } from "redux-router5";
 
+/**
+ * Calls saga with params when router transition succeeds.
+ * Provides all the routes to be activated.
+ * 
+ * @param {Generator} saga to be called
+ * @param {...any} params to be passed to saga
+ */
 export function* onRouteTransition(saga, ...params) {
   while (true) {
     const { payload: { route, previousRoute } } = yield take(
@@ -14,6 +21,13 @@ export function* onRouteTransition(saga, ...params) {
   }
 }
 
+/**
+ * Call saga when router transition succeedes on particular route.
+ *
+ * @param {String} route to match
+ * @param {Generator} saga to be called
+ * @param {...any} params to be passed to the saga
+ */
 export function* onRouteEnter(route, saga, ...params) {
   yield fork(onRouteTransition, function*(toActivate) {
     if (toActivate.some(activatedRoute => activatedRoute === route)) {
