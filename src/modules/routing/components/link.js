23c7565ec1f54e35b7df88cfcc98bacb2d3ac212
Guillaume Marty <guillaume@salsitasoft.com>
2017-11-06 15:49:45 +0100

    Styled link

--- src/modules/routing/components/link.js
+++ src/modules/routing/components/link.js
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { actions } from "redux-router5";
 
-export const Link = ({ onClick, children }) => (
-  <a onClick={onClick}>{children}</a>
+export const Link = ({ onClick, children, className }) => (
+  <a className={className} onClick={onClick}>
+    {children}
+  </a>
 );
 
 Link.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string
 };
 
 const mapDispatchToProps = {
