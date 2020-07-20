901fa46d3d1041339201436766deef419dabda8b
Dina Sushkova <dina@salsitasoft.com>
2020-07-20 11:44:43 +0200

    GH-36 Rewrite exercise 0 to typescript

---
+++ exercises/00-init/src/index.tsx
@@ -0,0 +1,8 @@
+import 'babel-polyfill'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Root } from 'modules/root/components/root'
+
+ReactDOM.render(<Root />, document.getElementById('root'))
