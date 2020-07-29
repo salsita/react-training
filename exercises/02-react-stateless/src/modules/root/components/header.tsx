12c6190177703fdadb55bbee4b38b72470e2e0bb
Dina Sushkova <dina@salsitasoft.com>
2020-07-29 11:28:16 +0200

    Add solutions for additonal exercises

--- exercises/02-react-stateless/src/modules/root/components/header.tsx
+++ exercises/02-react-stateless/src/modules/root/components/header.tsx
@@ -7,3 +7,24 @@ interface HeaderProps {
 export const Header: React.FC<HeaderProps> = ({ title }) => {
   return <h1>{title}</h1>
 }
+
+/////////////////////////////////////////////////////////////
+// Additional Exercises
+
+// 1. Header component created as a class that extends React.Component
+
+// export class Header extends React.Component<HeaderProps> {
+//   render() {
+//     console.log('Header.render');
+//     return <h1>{this.props.title}</h1>;
+//   }
+// }
+
+// 2. Header component created as a class that extends React.PureComponent
+
+// export class Header extends React.PureComponent<HeaderProps> {
+//   render() {
+//     console.log('Header.render');
+//     return <h1>{this.props.title}</h1>;
+//   }
+// }
