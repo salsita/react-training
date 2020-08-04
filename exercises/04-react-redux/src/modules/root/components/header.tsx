8a976045ac519df1d4b7748b78aa7d7748b1ebc9
Dina Sushkova <dina@salsitasoft.com>
2020-08-04 11:36:58 +0200

    Use React.FC instead of FC for typing

--- exercises/04-react-redux/src/modules/root/components/header.tsx
+++ exercises/04-react-redux/src/modules/root/components/header.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react'
+import React from 'react'
 
 import { useSelector } from 'react-redux'
 import { RootState } from '../root-reducer'
 
-export const Header: FC = () => {
+export const Header: React.FC = () => {
   const title = useSelector((state: RootState) => state.users.title)
 
   return <h1>{title}</h1>
