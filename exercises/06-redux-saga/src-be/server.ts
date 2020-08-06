2b1c60fed88d551b0cba6dca8d18f7737d45bc59
Dina Sushkova <dina@salsitasoft.com>
2020-08-06 12:28:27 +0200

    Rewrite BE with TypeScript, change BE file location
    BE is now in a separate directory with a separate tsconfig.json

---
+++ exercises/06-redux-saga/src-be/server.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import { User } from '../src/modules/users/user-types'
+
+const port = 3001
+const users: User[] = []
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/users', (_req, res) => res.json(users))
+
+app.post('/users', (req, res) => {
+  const { firstName, lastName } = req.body
+  const user = {
+    id: users.length + 1,
+    firstName,
+    lastName,
+  }
+
+  users.push(user)
+  res.json(user)
+})
+
+app.listen(port, () => console.log(`The server is running on port ${port}!`))
