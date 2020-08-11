3cd44240b5a1e983df9707f7b55ff305fd3bd515
Dina Sushkova <dina@salsitasoft.com>
2020-08-11 16:41:05 +0200

    Replace unnecessary <div> blocks with React Fragments

--- exercises/05-reselect/src/modules/root/components/root.tsx
+++ exercises/05-reselect/src/modules/root/components/root.tsx
@@ -4,8 +4,8 @@ import { Header } from './header'
 import { UserList } from 'modules/users/components/user-list'
 
 export const Root: React.FC = () => (
-  <div>
+  <>
     <Header />
     <UserList />
-  </div>
+  </>
 )
