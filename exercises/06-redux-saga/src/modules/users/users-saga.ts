74af04768596637963c1d4408a9628a75d2de20d
Dina Sushkova <dina@salsitasoft.com>
2020-08-17 15:52:41 +0200

    Add typings for Axios response payload

--- exercises/06-redux-saga/src/modules/users/users-saga.ts
+++ exercises/06-redux-saga/src/modules/users/users-saga.ts
@@ -3,10 +3,13 @@ import { AxiosResponse } from 'axios'
 
 import { usersActions } from 'modules/users/users-slice'
 import * as UsersEffects from 'modules/users/users-effects'
+import { User } from './user-types'
 
 function* getUsers() {
   try {
-    const { data: users }: AxiosResponse = yield call(UsersEffects.getUsers)
+    const { data: users }: AxiosResponse<User[]> = yield call(
+      UsersEffects.getUsers
+    )
     yield put(usersActions.usersLoaded(users))
   } catch (error) {
     console.log(error.message)
@@ -16,7 +19,7 @@ function* getUsers() {
 function* addUser(action: ReturnType<typeof usersActions.addUser>) {
   const { payload: user } = action
   try {
-    const response: AxiosResponse = yield call(UsersEffects.addUser, user)
+    const response: AxiosResponse<User> = yield call(UsersEffects.addUser, user)
 
     if (response) {
       yield fork(getUsers)
