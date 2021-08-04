3f14c6e4b0df3a6e0b5093348bb5298bbdfa4439
Thomas Morris <tmorris@salsitasoft.com>
2021-08-04 14:50:19 +0200

    add fetch (but don't put in redux yet) of users from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import { Header } from './components/Header';
 
 import './App.css';
@@ -10,6 +10,7 @@ import {Provider} from 'react-redux'
 import {userReducer} from "./store/reducers/userReducer";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import {EditPage} from "./components/EditPage";
+import axios from "axios";
 
 function App() {
   // createStore makes our reducer function able to be used in the wild
@@ -28,6 +29,25 @@ function App() {
           }
       ])
   }, [setUsers, users]);
+
+  useEffect(() => {
+      // fetch the users from http://localhost:7000/users
+      // axios
+      //     .get('http://localhost:7000/users')
+      //     .then((response) => {
+      //         console.log('the response is: ', response)
+      //         // stick the response in redux here
+      //     })
+      // console.log('Im saying this right after the axios call');
+
+      const fetchUsers = async () => {
+          const response = await axios.get('http://localhost:7000/users')
+          console.log(response, `<- response`)
+          // stick the response in redux here
+      }
+
+      fetchUsers();
+  }, [])
 
   return (
       <Provider store={store}>
--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -7,57 +7,7 @@ export interface UserState {
 }
 
 const initialState: UserState = {
-    users: [
-        {
-            id: 1,
-            firstName: 'Daenerys',
-            lastName: 'Targaeryn',
-            jobTitle: JobTitle.motherOfDragons,
-            age: 18
-        },
-        {
-            id: 2,
-            firstName: 'Jaime',
-            lastName: 'Lannister',
-            jobTitle: JobTitle.kingSlayer,
-            age: 30
-        },
-        {
-            id: 3,
-            firstName: 'Arya',
-            lastName: 'Stark',
-            jobTitle: JobTitle.noOne,
-            age: 16
-        },
-        {
-            id: 4,
-            firstName: 'Peter',
-            lastName: 'Baelish',
-            jobTitle: JobTitle.lord,
-            age: 45
-        },
-        {
-            id: 5,
-            firstName: 'Ned',
-            lastName: 'Stark',
-            jobTitle: JobTitle.lord,
-            age: 45
-        },
-        {
-            id: 6,
-            firstName: 'Ygritte',
-            lastName: 'I dont know',
-            jobTitle: JobTitle.wildling,
-            age: 23
-        },
-        {
-            id: 7,
-            firstName: 'Tormund',
-            lastName: 'Giantsbane',
-            jobTitle: JobTitle.wildling,
-            age: 36
-        },
-    ]
+    users: []
 }
 
 export const userReducer: Reducer<UserState, Action> = (state= initialState, action): UserState => {
